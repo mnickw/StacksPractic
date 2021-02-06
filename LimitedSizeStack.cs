9e45b116cdce8fd2d6e32d00433c231515c532c8
Nikita Minyuk <nikita.minyuk.w@gmail.com>
2021-02-06 07:24:22 +0300

    Initial project.

---
+++ LimitedSizeStack.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApplication
+{
+    public class LimitedSizeStack<T>
+    {
+        public LimitedSizeStack(int limit)
+        {
+        }
+
+        public void Push(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Pop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ ListModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoApplication
+{
+    public class ListModel<TItem>
+    {
+        public List<TItem> Items { get; }
+        public int Limit;
+
+        public ListModel(int limit)
+        {
+            Items = new List<TItem>();
+            Limit = limit;
+        }
+
+        public void AddItem(TItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(int index)
+        {
+            Items.RemoveAt(index);
+        }
+
+        public bool CanUndo()
+        {
+            return false;
+        }
+
+        public void Undo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
